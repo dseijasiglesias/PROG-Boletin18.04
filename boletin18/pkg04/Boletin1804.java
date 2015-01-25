d965da8e6ff79b98043bf04ce981417746e7dbef
Seijas <Seijas@192.168.1.129>
2015-01-25 21:49:22 +0100

    Hiper compacto y nuevo formato de presentacion de DNI

--- boletin18/pkg04/Boletin1804.java
+++ boletin18/pkg04/Boletin1804.java
@@ -6,12 +6,11 @@ public class Boletin1804 {
 
     public static void main(String[] args) {
         
-        int dni;
         char [] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
         
-        dni = Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de DNI sin la letra"));
+        int dni = Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de DNI sin la letra"));
         
-        JOptionPane.showMessageDialog(null, "Su D.N.I. es: " + dni + letras[dni%23]);
+        JOptionPane.showMessageDialog(null, "Su D.N.I. es: " + dni + "-" + letras[dni%23]);
     }
     
 }
